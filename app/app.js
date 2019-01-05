e11753e43821b7d6e25be31875dc1b7c1ee0f92c
Austin Stotts <austintstotts@gmail.com>
2019-01-04 19:26:30 -0600

    pet_id is now in params

--- app/app.js
+++ app/app.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/buy', (req, res) => {
-  const id = req.headers.pet_id
+app.get('/buy/:pet_id', (req, res) => {
+  const id = req.params.pet_id
   console.log('\n -> Get Request on "/buy"\n -> pet_id: ', id);
   db.getPetByID(id, (err, data) => {
     // if err || data object is empty
